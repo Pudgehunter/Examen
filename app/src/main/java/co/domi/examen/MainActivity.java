e9a2b999f999382a3aaf6c99cb2c4aa3c0e95cc0
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2020-09-18 00:51:21 -0500

    Completo

--- app/src/main/java/co/domi/examen/MainActivity.java
+++ app/src/main/java/co/domi/examen/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button registrarButton;
     TextView nombreView;
+    ArrayList<String> numeros;
 
 
     @Override
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         registrarButton = findViewById(R.id.registrarButton);
         nombreView = findViewById(R.id.nombreView);
         nombreView.setText(" ");
+        numeros = new ArrayList<String>();
 
 
 
@@ -29,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
                 (v) -> {
                     Intent i = new Intent(this,RegistrarClass.class);
                     startActivity(i);
-                    finish();
                 }
         );
     }
@@ -38,20 +42,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         SharedPreferences preferences = getSharedPreferences("savePerson",MODE_PRIVATE);
-        String username = preferences.getString("registros","Yoshiki Tsutsui Michida");
+        String username = preferences.getString("registros","Nombres");
         String[] partes = username.split(":");
         int Riesgo = preferences.getInt("riesgoTotal",0);
+        numeros.add(String.valueOf(Riesgo));
+
+        Log.e("numeros",numeros.toString());
        // String total = preferences.getString("riesgoStringTotal","0");
        // String RiesgoPartes = total + ":" + Riesgo;
         //String[] RiesgoPartesTotales = RiesgoPartes.split(":");
-        for(int i = 0; i < partes.length; i++) {
-            nombreView.append("" + partes[i] + ":" + " " + Riesgo + "\n");
-        }
+
+        nombreView.append("" + partes[partes.length-1] + ":" + " " + Riesgo + "\n");
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        getSharedPreferences("savePerson", MODE_PRIVATE).edit().clear().apply();
+       // getSharedPreferences("savePerson", MODE_PRIVATE).edit().clear().apply();
     }
 }
